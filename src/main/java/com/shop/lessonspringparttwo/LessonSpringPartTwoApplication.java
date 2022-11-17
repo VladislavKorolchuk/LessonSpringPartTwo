64ae9f2d998068431470ea672e2bc1be039d4d3a
vladislavkorolcuk <9329496@mail.ru>
2022-11-17 20:15:18 +0300

    Initial commit

---
+++ src/main/java/com/shop/lessonspringparttwo/LessonSpringPartTwoApplication.java
@@ -0,0 +1,13 @@
+package com.shop.lessonspringparttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonSpringPartTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonSpringPartTwoApplication.class, args);
+	}
+
+}
